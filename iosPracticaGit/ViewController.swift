ee15cf7a35baa2c3d383a14a3a3bfefaee6d556a
Resembrink Correa <resembrink.correa@gmail.com>
2019-08-04 17:27:11 -0500

    inicio commit bit sourcetree

---
+++ iosPracticaGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosPracticaGit
+//
+//  Created by Resembrink Correa Egoavil on 8/4/19.
+//  Copyright © 2019 Resembrink Correa Egoavil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
